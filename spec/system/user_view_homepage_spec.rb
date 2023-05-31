8c9a41222c07437c7f67b59bcddb39d394baf0ff
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-31 15:25:33 -0400

    HomePage

---
+++ spec/system/user_view_homepage_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Usuário visita homepage' do
+  it "e vê título da página" do
+    
+    visit root_path
+
+    expect(page).to have_content "Projeto Star Wars"  
+  end
+  
+  it "e vê planetas cadastrados" do
+
+    visit root_path
+
+    expect(page).to have_content "Tatooine"
+    expect(page).to have_content "Alderaan"
+    expect(page).to have_content "Yavin IV"
+    expect(page).to have_content "Hoth"
+    expect(page).to have_content "Dagobar"
+    expect(page).to have_content "Naboo"
+
+    
+  end
+  
+  
+end
